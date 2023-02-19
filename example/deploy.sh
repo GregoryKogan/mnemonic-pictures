2d41b49b5facda4ed731aae16119bc0d97f21519
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-02-20 02:13:37 +0300

    deploy script added

---
+++ example/deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn build
+
+# navigate into the build output directory
+cd dist
+
+cp index.html 404.html
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:GregoryKogan/mnemonic-pictures.git main:gh-pages
+
+cd -
